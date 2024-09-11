06d2a96682428d22a0e103d7d6361d22e8058a3e
Heshan Nanayakkara <kalaruheshan@gmail.com>
2024-09-11 16:29:30 +0530

    Update s3_to_dynamodb.py

--- Lambda/s3_to_dynamodb.py
+++ Lambda/s3_to_dynamodb.py
@@ -15,30 +15,24 @@ dynamodb = boto3.resource('dynamodb')
 # and writes to dynamo db table
 def lambda_handler(event, context):
     try:
-        # Get the bucket name
-        # and the file path of the json file which was uploaded to trigger lambda function
-        bucket = event['Records'][0]['s3']['bucket']['name']
-        json_file_name = event['Records'][0]['s3']['object']['key']
+        # Get address data from S3
+        bucket = 'input-data-dwh'
+        json_file_name = 'dataset/address.json'
+
+        json_object = s3_client.get_object(
+            Bucket=bucket,
+            Key=json_file_name
+        )
+        
+        address_data_dict = json.loads(json_object['Body'].read())
         
         
-        # Check whether the expected json file was uploaded to S3 bucket
-        if(bucket == 'input-data-dwh' and json_file_name == 'dataset/address.json'):
-            # Read address data from S3 bucket
-            json_object = s3_client.get_object(
-                Bucket=bucket,
-                Key=json_file_name
-            )
-            
-            address_data_dict = json.loads(json_object['Body'].read())
-            
-            
-            # Write address data to dynamo db table
-            address_table = dynamodb.Table('Address')
-            
-            # Write items seperately to table in dynamo db
-            for i in range(len(address_data_dict['Addresses'])):
-                address_table.put_item(Item=address_data_dict['Addresses'][i])
-            
+        # Write address data to dynamo db table
+        address_table = dynamodb.Table('Address')
+
+        for i in range(len(address_data_dict['Addresses'])):
+            address_table.put_item(Item=address_data_dict['Addresses'][i])
+        
         return "Success"
     except Exception as e:
         print(f"An error occurred: {e}")
